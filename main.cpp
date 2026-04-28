af3991c0e8b9db5f844a59ddd6b8f945c18b2a83
Vilimas Vaitulevičius <s062523@ad.viko.lt>
2026-04-28 12:42:10 +0300

    Pataisiau meniu ir pradejau skaiciavimus

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
-
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 using namespace std;
 
-// Struktūra meniu elementams
 struct menuItemType {
     string menuItem;
     double menuPrice;
 };
 
-// Funkcija nuskaito meniu iš failo
 void getData(menuItemType menuList[], int &kiek) {
     ifstream fin("menu.txt");
 
@@ -21,7 +18,6 @@ void getData(menuItemType menuList[], int &kiek) {
 
     kiek = 0;
 
-    // Skaitome iki failo pabaigos
     while (!fin.eof()) {
         fin >> ws;
         getline(fin, menuList[kiek].menuItem);
@@ -36,4 +32,52 @@ void getData(menuItemType menuList[], int &kiek) {
     }
 
     fin.close();
+}
+
+void showMenu(menuItemType menuList[], int kiek) {
+    cout << "\n--- PUSRYČIŲ MENIU ---\n";
+
+    for (int i = 0; i < kiek; i++) {
+        cout << i + 1 << ". " << menuList[i].menuItem
+             << "  " << fixed << setprecision(2)
+             << menuList[i].menuPrice << "€\n";
+    }
+
+    cout << "\nPasirinkite patiekalo numerį (0 - baigti užsakymą)\n";
+}
+
+void printCheck(menuItemType menuList[], int kiek, int pasirinkimai[], int porcijos[]) {
+    ofstream fout("receipt.txt");
+
+    double suma = 0;
+
+    cout << "\nSveiki atvykę į restoraną „Pavadinimas“\n\n";
+    fout << "Sveiki atvykę į restoraną „Pavadinimas“\n\n";
+
+    for (int i = 0; i < kiek; i++) {
+        if (pasirinkimai[i] == 1) {
+
+            double tarpine = menuList[i].menuPrice * porcijos[i];
+            suma += tarpine;
+
+            cout << porcijos[i] << "  " << menuList[i].menuItem
+                 << "   " << fixed << setprecision(2)
+                 << tarpine << "€\n";
+
+            fout << porcijos[i] << "  " << menuList[i].menuItem
+                 << "   " << fixed << setprecision(2)
+                 << tarpine << "€\n";
+        }
+    }
+
+    double pvm = suma * 0.21;
+    double galutine = suma + pvm;
+
+    cout << "\nMokesčiai (21%)   " << fixed << setprecision(2) << pvm << "€\n";
+    cout << "Galutinė suma     " << fixed << setprecision(2) << galutine << "€\n";
+
+    fout << "\nMokesčiai (21%)   " << fixed << setprecision(2) << pvm << "€\n";
+    fout << "Galutinė suma     " << fixed << setprecision(2) << galutine << "€\n";
+
+    fout.close();
 }
